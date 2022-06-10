96a2f189cb1319235aca3fd21410a73d822b5bd3
JulietteLajoinie <juliette.lajoinie@toulouse.miage.fr>
2022-06-10 08:15:15 +0200

    Création du projet

---
+++ src/main/java/com/fr/miage/toulouse/m2/lajoinie/juliette/odorugestionbadge/OdoruGestionBadgeApplication.java
@@ -0,0 +1,13 @@
+package com.fr.miage.toulouse.m2.lajoinie.juliette.odorugestionbadge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdoruGestionBadgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdoruGestionBadgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fr/miage/toulouse/m2/lajoinie/juliette/odorugestionbadge/OdoruGestionBadgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fr.miage.toulouse.m2.lajoinie.juliette.odorugestionbadge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdoruGestionBadgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
